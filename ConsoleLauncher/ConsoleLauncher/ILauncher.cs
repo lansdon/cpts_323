f3f6178ee0e8d6771a22078906d85ccbd230f696
Tweety79rw <ryan.wilson2@email.wsu.edu>
2014-02-11 00:08:41 -0800

    start of launcher interface and implementation

---
+++ ConsoleLauncher/ConsoleLauncher/ILauncher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLauncher
+{
+    public interface ILauncher
+    {
+        void moveUp();
+        void moveDown();
+        void moveLeft();
+        void moveRight();
+        void moveBy(double x, double y, double z);
+        void moveTo(double phi, double thata);
+        void fire();
+        void fireAt(double x, double y, double z);
+        void calibrate();
+    }
+}
--- ConsoleLauncher/ConsoleLauncher/Program.cs
+++ ConsoleLauncher/ConsoleLauncher/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-
+using ConsoleLauncher;
 
 
 namespace LauncherClient
@@ -14,8 +14,17 @@ namespace LauncherClient
         static void Main(string[] args)
         {
             // Start the controller class which drives the entire program.
-            Controller controller = new Controller();
-            controller.Run(args);
+            //Controller controller = new Controller();
+            //controller.Run(args);
+            missileLauncher gun = new missileLauncher();
+            gun.moveBy(-11,-10,5);
+            gun.fire();
+            gun.moveTo(200, 0);
+            gun.fire();
+            //gun.fireAt(1,5, 4);
+            //gun.fireAt(1, 5, 1);
+            //gun.fireAt(1,5, -3);
+
         }
     }
 }
