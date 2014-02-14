1453f9acda8927295a1526a958ff31bc2b46b255
Ahlywog <Ahlywog@gmail.com>
2014-02-13 18:43:25 -0800

    Factory Class!  Prolly needs work.
    Generic factory class.   Still has the test classes in it + main.
    Remove as needed.  if anything more needs to be added let me knizzow.

---
+++ Production/Src/SadLibrary/FileLoader/Factory Class.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Factory_Class
+{
+    class Program
+    {
+        abstract public class FileTargetLoader
+        {
+            public FileTargetLoader(string FilePath)
+            {
+                Console.WriteLine(FilePath);
+            }
+            // This is just for my coding sake.  Remove later when no longer needed.
+        }
+
+        public class FileTargetLoaderIni : FileTargetLoader
+        {
+            public FileTargetLoaderIni(string FilePath): base (FilePath)
+            {
+                Console.WriteLine(FilePath);
+            }
+            // This is just for my coding sake.  Remove later when no longer needed.
+        }
+
+        public class Target
+        {
+            public Target() { }
+            // This is just for my coding sake.  Remove later when no longer needed.
+        }
+
+        public class Object_Factory
+        {
+            public Object_Factory() { }
+
+            public FileTargetLoader getParser(string FilePath)
+            {
+                FileTargetLoader temp = null;
+                switch (Path.GetExtension(FilePath).ToLower())
+                {
+                    case ".ini":
+                        temp = (FileTargetLoader)(new FileTargetLoaderIni(FilePath));
+                        break;
+                    case ".txt":
+                        break;
+                    case ".xml":
+                        break;
+                    default:
+                        Console.WriteLine("Uknown file extension: " + Path.GetExtension(FilePath));
+                        break;
+                }
+                return temp;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Object_Factory Factory = new Object_Factory();
+
+            FileTargetLoader Temp = Factory.getParser("blubber.ini");
+
+            Console.WriteLine("Press Enter to continue...");
+            Console.ReadLine();
+        }
+    }
+}
