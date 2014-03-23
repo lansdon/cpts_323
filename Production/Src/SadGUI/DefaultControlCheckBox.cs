0ae81b21d844ccdb1e127d48a88edfadc5d624ce
Ahlywog <Ahlywog@gmail.com>
2014-03-22 21:25:35 -0700

    Update 3/22/2014 9:25pm
    Changes to Check Box stuffs.

---
+++ Production/Src/SadGUI/DefaultControlCheckBox.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SadGUI
+{
+    public class DefaultControlCheckBox: ViewModelBase
+    {
+        private bool IsChecked;
+        public DefaultControlCheckBox()
+        {
+            IsChecked = false;
+        }
+
+        public bool DefaultControl_CheckBox_IsChecked
+        {
+            get
+            {
+                return IsChecked;
+            }
+            set
+            {
+                IsChecked = value;
+                Process_CheckBox();
+            }
+        }
+
+        private void Process_CheckBox()
+        {
+            if (IsChecked == true)
+                IsChecked = !IsChecked;
+        }
+    }
+}
---
+++ Production/Src/SadGUI/TwitterFeedCheckBox.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SadGUI
+{
+    public class TwitterFeedCheckBox: ViewModelBase
+    {
+        private bool IsChecked;
+
+        public TwitterFeedCheckBox()
+        {
+            IsChecked = false;
+        }
+
+        public bool TwitterFeed_CheckBox_Checked
+        {
+            get
+            {
+                return IsChecked;
+            }
+            set
+            {
+                IsChecked = value;
+                Process_CheckBox();
+            }
+        }
+
+        private void Process_CheckBox()
+        {
+            if (IsChecked == true)
+                IsChecked = !IsChecked;
+        }
+    }
+}
