9489d04136d7fbbcfc52053de536274f1a1fa536
Tweety79rw <ryan.wilson2@email.wsu.edu>
2014-04-22 12:57:27 -0700

    Game selection version 0.1

--- Production/Src/SadGUI/ServerViewModel.cs
+++ Production/Src/SadGUI/ServerViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SadGUI.mizaWindows;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +13,15 @@ namespace SadGUI
     class ServerViewModel : ViewModelBase
     {
         private string _serverIP;
+        private string _serverPort;
+        public ObservableCollection<string> games { get; set; }
         public ServerViewModel()
         {
-
+            games = new ObservableCollection<string>();
+            games.Add("game 1");
+            games.Add("game 2");
+            games.Add("game 3");
+            games.Add("game 4");
             OkCommand = new DelegateCommand(Ok);
             CancelCommand = new DelegateCommand(Cancel);
         }
@@ -26,10 +34,20 @@ namespace SadGUI
                 OnPropertyChanged("serverIP");
             }
         }
+        public string serverPort
+        {
+            get { return _serverPort; }
+            set
+            {
+                _serverPort = value;
+                OnPropertyChanged("serverPort");
+            }
+        }
         public void Ok()
         {
             //do something special.
-            ServerCheckBox.instance.ServerControl_CheckBox_IsChecked = false;
+            //ServerCheckBox.instance.ServerControl_CheckBox_IsChecked = false;
+            ContentController.SetContentToController("RightCheckBoxPanel", new gameSelectionView());
         }
         public void Cancel()
         {
