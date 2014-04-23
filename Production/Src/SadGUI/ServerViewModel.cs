dbcbc017d5f8c78af27ec82fbc6f163d80fb3455
Tweety79rw <ryan.wilson2@email.wsu.edu>
2014-04-23 13:25:39 -0700

    game selection version 1.1
    added error checking if server doesn't connect.

--- Production/Src/SadGUI/ServerViewModel.cs
+++ Production/Src/SadGUI/ServerViewModel.cs
@@ -18,12 +18,32 @@ namespace SadGUI
     {
         private string _serverIP;
         private string _serverPort;
-        
+        private string _serverIPLabel;
+        private string _serverPortLabel;
         public ServerViewModel()
         {
-            
+            serverIPLabel = "Input Server IP:";
+            serverPortLabel = "Input Server Port:";
             OkCommand = new DelegateCommand(Ok);
             CancelCommand = new DelegateCommand(Cancel);
+        }
+        public string serverIPLabel
+        {
+            get { return _serverIPLabel; }
+            set
+            {
+                _serverIPLabel = value;
+                OnPropertyChanged("serverIPLabel");
+            }
+        }
+        public string serverPortLabel
+        {
+            get { return _serverPortLabel; }
+            set
+            {
+                _serverPortLabel = value;
+                OnPropertyChanged("serverPortLabel");
+            }
         }
         public string serverIP
         {
@@ -53,16 +73,19 @@ namespace SadGUI
             else
                 serverType = GameServerType.WebClient;
 
-            if ((gameServer = GameServerFactory.Create(serverType, "Team Mizu!!", _serverIP, Convert.ToInt32(_serverPort))) != null)
+            try
             {
+                gameServer = GameServerFactory.Create(serverType, "Team Mizu!!", _serverIP, Convert.ToInt32(_serverPort));
                 Mediator.Instance.SendMessage("to games", gameServer);
                 ContentController.SetContentToController("RightCheckBoxPanel", new gameSelectionView());
             }
-            else
+            catch
             {
                 serverIP = null;
                 serverPort = null;
                 // change label to reflect that server didn't connect
+                serverIPLabel = "Error, Please try again, Server IP:";
+                serverPortLabel = "Error, Please try again, Server Port:";
             }
             
 
