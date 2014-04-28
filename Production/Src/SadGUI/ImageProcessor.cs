dd68cd598eb7785c664359faa8c0d75d73166b88
Lansdon Page <lansdon.page@email.wsu.edu>
2014-04-27 22:11:36 -0700

    Trying a new detection method.

--- Production/Src/SadGUI/ImageProcessor.cs
+++ Production/Src/SadGUI/ImageProcessor.cs
@@ -188,23 +188,43 @@ namespace SadGUI
                 //}
 
                 // FOES
-                var detectedFoes = grayFrame.DetectHaarCascade(foeHaarCascade)[0];
-                foreach (var foe in detectedFoes)
+                //var detectedFoes = grayFrame.DetectHaarCascade(foeHaarCascade)[0];
+                //foreach (var foe in detectedFoes)
+                //{
+                //    pos = PositionFromFrame(foe.rect);
+                //    image.Draw(foe.rect, new Bgr(0, 0, double.MaxValue), 3);
+                //    DrawPositionText(ref image, foe.rect, pos);
+                //}
+
+                //// FRIENDS
+                //var detectedFriends = grayFrame.DetectHaarCascade(friendHaarCascade)[0];
+                //foreach (var friend in detectedFriends)
+                //{
+                //    pos = PositionFromFrame(friend.rect);
+                //    image.Draw(friend.rect, new Bgr(0, double.MaxValue, 0), 3);
+                //    DrawPositionText(ref image, friend.rect, pos);
+                //}
+
+ //               Image<Bgr, byte> source = new Image<Bgr, byte>(filepathB); // Image B
+                Image<Bgr, byte> template = new Image<Bgr, byte>("foe.png"); // Image A
+                Image<Bgr, byte> imageToShow = image.Copy();
+
+                using (Image<Gray, float> result = image.MatchTemplate(template, Emgu.CV.CvEnum.TM_TYPE.CV_TM_CCOEFF_NORMED))
                 {
-                    pos = PositionFromFrame(foe.rect);
-                    image.Draw(foe.rect, new Bgr(0, 0, double.MaxValue), 3);
-                    DrawPositionText(ref image, foe.rect, pos);
+                    double[] minValues, maxValues;
+                    Point[] minLocations, maxLocations;
+                    result.MinMax(out minValues, out maxValues, out minLocations, out maxLocations);
+
+                    // You can try different values of the threshold. I guess somewhere between 0.75 and 0.95 would be good.
+                    if (maxValues[0] > 0.9)
+                    {
+                        // This is a match. Do something with it, for example draw a rectangle around it.
+                        Rectangle match = new Rectangle(maxLocations[0], template.Size);
+                        imageToShow.Draw(match, new Bgr(Color.Red), 3);
+                    }
                 }
 
-                // FRIENDS
-                var detectedFriends = grayFrame.DetectHaarCascade(friendHaarCascade)[0];
-                foreach (var friend in detectedFriends)
-                {
-                    pos = PositionFromFrame(friend.rect);
-                    image.Draw(friend.rect, new Bgr(0, double.MaxValue, 0), 3);
-                    DrawPositionText(ref image, friend.rect, pos);
-                }
-  
+
             }
         }
 
